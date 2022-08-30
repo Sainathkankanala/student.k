88a12ed5ca5bfa0a4106734f6af19efae05892de
Rojapandiaraj <112462812+Rojapandiaraj@users.noreply.github.com>
2022-08-30 01:47:46 -0700

    Add files via upload

---
+++ Integer.java
@@ -0,0 +1,12 @@
+import java.util.*;
+ 
+class GFG 
+{
+
+    public static void main(String[] args)
+    {
+        int i = 65;
+             char ch = (char)i;
+             System.out.println(ch);
+    }
+}
